c4e52d6db253bbc8eee67f4edd5dec954d15aaba
kuba <kubadt@gmail.com>
2020-11-17 11:46:16 +0100

    Testy Jednostkowe

---
+++ PodstawoweTestySerwera/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PodstawoweTestySerwera")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PodstawoweTestySerwera")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e421dab-d923-4317-a85b-d8b538ca4bc2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PodstawoweTestySerwera/ZestawTestowTworzeniaSerwera.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerEchoLibrary;
+
+namespace PodstawoweTestySerwera
+{
+    [TestClass]
+    public class ZestawTestowTworzeniaSerwera
+    {
+        [TestMethod]
+        public void TestowanieBlednegoNumeruPortu()
+        {
+            try
+            {
+                Server<LoginServerProtocol> server = new ServerTAP<LoginServerProtocol>(IPAddress.Parse("127.0.0.1"), 80);
+                server.Start();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        [TestMethod]
+        public void TestowanieBlednegoAdresuIP()
+        {
+            try
+            {
+                Server<LoginServerProtocol> server = new ServerTAP<LoginServerProtocol>(IPAddress.Parse("127.0.0"), 3000);
+                server.Start();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
